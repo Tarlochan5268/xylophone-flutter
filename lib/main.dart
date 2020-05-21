d91da9055009aaddbb1640e133c607bd4e4cb192
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-05-20 21:43:57 -0400

    made column and added row and given colour to it

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,50 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
-class XylophoneApp extends StatelessWidget {
+class XylophoneApp extends StatefulWidget {
+  @override
+  _XylophoneAppState createState() => _XylophoneAppState();
+}
+
+class _XylophoneAppState extends State<XylophoneApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                color: Colors.red,
+                height: 90,
+              ),
+              Container(
+                color: Colors.orange,
+                height: 90,
+              ),
+              Container(
+                color: Colors.yellow,
+                height: 90,
+              ),
+              Container(
+                color: Colors.green,
+                height: 90,
+              ),
+              Container(
+                color: Colors.teal,
+                height: 90,
+              ),
+              Container(
+                color: Colors.blueAccent,
+                height: 90,
+              ),
+              Container(
+                color: Colors.purple,
+                height: 90,
+              ),
+            ],
+          ),
         ),
       ),
     );
