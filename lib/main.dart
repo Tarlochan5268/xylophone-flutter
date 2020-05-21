e7b09271e764ee1d84052e390ab923222aa92fd9
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-05-21 16:18:09 -0400

    added Audio player and sound on click of each colour

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -16,33 +17,75 @@ class _XylophoneAppState extends State<XylophoneApp> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Container(
-                color: Colors.red,
-                height: 90,
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note1.wav');
+                },
+                child: Container(
+                  color: Colors.red,
+                  height: 90,
+                ),
               ),
-              Container(
-                color: Colors.orange,
-                height: 90,
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note2.wav');
+                },
+                child: Container(
+                  color: Colors.orange,
+                  height: 90,
+                ),
               ),
-              Container(
-                color: Colors.yellow,
-                height: 90,
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note3.wav');
+                },
+                child: Container(
+                  color: Colors.yellow,
+                  height: 90,
+                ),
               ),
-              Container(
-                color: Colors.green,
-                height: 90,
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note4.wav');
+                },
+                child: Container(
+                  color: Colors.green,
+                  height: 90,
+                ),
               ),
-              Container(
-                color: Colors.teal,
-                height: 90,
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note5.wav');
+                },
+                child: Container(
+                  color: Colors.teal,
+                  height: 90,
+                ),
               ),
-              Container(
-                color: Colors.blueAccent,
-                height: 90,
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note6.wav');
+                },
+                child: Container(
+                  color: Colors.blueAccent,
+                  height: 90,
+                ),
               ),
-              Container(
-                color: Colors.purple,
-                height: 90,
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note7.wav');
+                },
+                child: Container(
+                  color: Colors.purple,
+                  height: 90,
+                ),
               ),
             ],
           ),
